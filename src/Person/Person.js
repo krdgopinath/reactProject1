7d5d7bf8d02852fc12b4d3b766017e8ffbcd0e41
Gopinath <krdgopinath@gmail.com>
2020-03-09 07:59:24 +0530

    react-project1 - change 2

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,6 +4,7 @@ const person= (props) => {
     return(
        <div>
             <p onClick={props.click}>Welcome to reactJS {props.name } {props.age} </p>
+            <h1>heading1</h1>
             <input type="text" onChange={props.changed}></input>
         </div>
     );
